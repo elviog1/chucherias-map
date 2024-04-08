358f1a2eadd1634312f05faf58a9b389b7638783
Elvio Galeano <elvio.galeano827@gmail.com>
2024-04-08 18:06:46 -0300

    finish

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ var foodIcon = L.icon({
   iconUrl: "../images/food.png",
   iconSize: [32, 32], // Tamaño del icono [ancho, alto]
 });
+var manIcon = L.icon({
+  iconUrl: "../images/man.png",
+  iconSize: [32, 32], // Tamaño del icono [ancho, alto]
+});
+var womanIcon = L.icon({
+  iconUrl: "../images/woman.png",
+  iconSize: [32, 32], // Tamaño del icono [ancho, alto]
+});
+var workIcon = L.icon({
+  iconUrl: "../images/work.png",
+  iconSize: [32, 32], // Tamaño del icono [ancho, alto]
+});
 
 //funcion para agregar un marcador al mapa
 function addMarkerToMap(latlng, popupContent, category) {
@@ -46,6 +58,12 @@ function addMarkerToMap(latlng, popupContent, category) {
     icon = lightIcon;
   } else if (category === "Comidas") {
     icon = foodIcon;
+  } else if (category === "Trabajo") {
+    icon = workIcon;
+  } else if (category === "Hombre") {
+    icon = manIcon;
+  } else if (category === "Mujer") {
+    icon = womanIcon;
   } else if (category === "Otros") {
     icon = otherIcon;
   }
@@ -80,14 +98,16 @@ function onMapClick(e) {
 function createPopupForm(marker) {
   var form = "<h3 class='popup-title'>Información del Marcador</h3>";
   form += '<form onsubmit="saveMarkerInfo(event, ' + L.stamp(marker) + ')">';
-  form += '<input class="form-popup-input" placeholder="Titulo" type="text" name="title" required><br>';
+  form +=
+    '<input class="form-popup-input" placeholder="Titulo" type="text" name="title" required><br>';
   form +=
     '<br><textarea placeholder="Descripcion" name="description" rows="4" required maxlength="100"></textarea><br>';
   form +=
-    'Categoría:<select class="form-popup-input" name="category"><option value="Electricidad">Electricidad</option><option value="Peligro">Peligro</option><option value="Comidas">Comidas</option><option value="Inseguridad">Inseguridad</option><option value="Otros">Otros</option></select><br>';
-  form += '<div class="form-popup-buttons"><input class="popup-button save-button" type="submit" value="Guardar"><button class="popup-button delete-mark" onclick="deleteMarker(' +
-  L.stamp(marker) +
-  ')">Eliminar Marcador</button></div>';
+    'Categoría:<select class="form-popup-input" name="category"><option value="Comidas">Comidas</option><option value="Electricidad">Electricidad</option><option value="Hombre">Hombre</option><option value="Inseguridad">Inseguridad</option><option value="Mujer">Mujer</option><option value="Peligro">Peligro</option><option value="Trabajo">Trabajo</option><option value="Otros">Otros</option></select><br>';
+  form +=
+    '<div class="form-popup-buttons"><input class="popup-button save-button" type="submit" value="Guardar"><button class="popup-button delete-mark" onclick="deleteMarker(' +
+    L.stamp(marker) +
+    ')">Eliminar Marcador</button></div>';
   form += "</form>";
   return form;
 }
@@ -99,10 +119,10 @@ function saveMarkerInfo(event, markerId) {
   let title = event.target.title.value;
   let category = event.target.category.value;
   let description = event.target.description.value;
-  let idMark = markerId
+  let idMark = markerId;
 
   let data = {
-    idMark:idMark,
+    idMark: idMark,
     title: title,
     category: category,
     lat: marker.getLatLng().lat,
@@ -114,7 +134,9 @@ function saveMarkerInfo(event, markerId) {
       category +
       "</p><p><strong>Descripción:</strong> " +
       description +
-      "</p><button class='popup-button-delete delete-mark' onclick=deleteMarkFromDB(" + markerId + ")>Eliminar</button>",
+      "</p><button class='popup-button-delete delete-mark' onclick=deleteMarkFromDB(" +
+      markerId +
+      ")>Eliminar</button>",
   };
 
   fetch("https://chucherias-map-server-one.vercel.app/marks", {
@@ -131,7 +153,7 @@ function saveMarkerInfo(event, markerId) {
       throw new Error("Error al guardar el marcador");
     })
     .then((savedMarker) => {
-      deleteMarker(idMark)
+      deleteMarker(idMark);
       addMarkerToMap(
         { lat: savedMarker.lat, lng: savedMarker.lng },
         savedMarker.popupContent,
@@ -141,36 +163,36 @@ function saveMarkerInfo(event, markerId) {
     .catch((error) => console.log(error));
 }
 
-function deleteMarkFromDB(mark_id){
-    fetch(`https://chucherias-map-server-one.vercel.app/marks/${mark_id}`,{
-        method:"DELETE"
-    })
-    .then(response => {
-        if(response.ok){
+function deleteMarkFromDB(mark_id) {
+  fetch(`https://chucherias-map-server-one.vercel.app/marks/${mark_id}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        Swal.fire({
+          title: "¿Estas seguro?",
+          text: "La marca se eliminara completamente",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Si",
+        }).then((result) => {
+          if (result.isConfirmed) {
             Swal.fire({
-                title: "¿Estas seguro?",
-                text: "La marca se eliminara completamente",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#3085d6",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Si",
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  Swal.fire({
-                    title: "Eliminado!",
-                    text: "Marca eliminada exitosamente.",
-                    icon: "success"
-                  });
-                  markers.clearLayers();
-            fetchAndShowMarkers()
-                }
-              });
-        }else{
-            console.log("Error al eliminar la marca")
-        }
+              title: "Eliminado!",
+              text: "Marca eliminada exitosamente.",
+              icon: "success",
+            });
+            markers.clearLayers();
+            fetchAndShowMarkers();
+          }
+        });
+      } else {
+        console.log("Error al eliminar la marca");
+      }
     })
-    .catch(error => console.log(error))
+    .catch((error) => console.log(error));
 }
 
 // Función para eliminar un marcador
@@ -183,90 +205,117 @@ function deleteMarker(markerId) {
 }
 
 // Obtener todas las imágenes de las categorías
-var categoryImages = document.querySelectorAll('.icon-mark');
+var categoryImages = document.querySelectorAll(".icon-mark");
 
 // Agregar un controlador de eventos a cada imagen
-categoryImages.forEach(function(image) {
-    image.addEventListener('click', function(event) {
-        // Obtener la categoría de la imagen
-        var category = event.target.alt.split(' ')[0]; // Tomar la primera palabra de la descripción de la imagen
+categoryImages.forEach(function (image) {
+  image.addEventListener("click", function (event) {
+    // Obtener la categoría de la imagen
+    var category = event.target.alt.split(" ")[0]; // Tomar la primera palabra de la descripción de la imagen
 
-        // Eliminar todas las marcas del mapa
-        markers.clearLayers();
+    // Eliminar todas las marcas del mapa
+    markers.clearLayers();
 
-        // Filtrar y mostrar solo las marcas de la categoría seleccionada
-        fetch("https://chucherias-map-server-one.vercel.app/marks")
-            .then((response) => response.json())
-            .then((marks) => {
-                if(category === "Todos"){
-                    fetchAndShowMarkers()
-                }else{
-                    marks.forEach((mark) => {
-                        if (mark.category === category) {
-                            var latlng = { lat: mark.lat, lng: mark.lng };
-                            var popupContent = mark.popupContent;
-                            addMarkerToMap(latlng, popupContent, category);
-                        }
-                    });
-                }
-            })
-            .catch((error) => console.log(error));
-    });
+    // Filtrar y mostrar solo las marcas de la categoría seleccionada
+    fetch("https://chucherias-map-server-one.vercel.app/marks")
+      .then((response) => response.json())
+      .then((marks) => {
+        if (category === "Todos") {
+          fetchAndShowMarkers();
+        } else {
+          marks.forEach((mark) => {
+            if (mark.category === category) {
+              var latlng = { lat: mark.lat, lng: mark.lng };
+              var popupContent = mark.popupContent;
+              addMarkerToMap(latlng, popupContent, category);
+            }
+          });
+        }
+      })
+      .catch((error) => console.log(error));
+  });
 });
 
-document.getElementById('city-search').addEventListener('input', function() {
+document.getElementById("city-search").addEventListener("input", function () {
   var searchInput = this.value.trim();
   if (searchInput.length > 0) {
-      searchCities(searchInput);
+    searchCities(searchInput);
   } else {
-      clearSearchResults();
+    clearSearchResults();
   }
 });
 
-document.addEventListener('click', function(event) {
-  var searchResultsContainer = document.getElementById('search-results');
+document.addEventListener("click", function (event) {
+  var searchResultsContainer = document.getElementById("search-results");
   var isClickInsideContainer = searchResultsContainer.contains(event.target);
 
   if (!isClickInsideContainer) {
-      clearSearchResults();
+    clearSearchResults();
+  }
+});
+
+document.getElementById("search-button").addEventListener("click", function () {
+  let cityName = document.getElementById("city-search").value.trim();
+  if (cityName.length > 0) {
+    fetch(
+      `https://nominatim.openstreetmap.org/search?format=json&q=${cityName}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data && data.length > 0) {
+          let lat = parseFloat(data[0].lat);
+          let lon = parseFloat(data[0].lon);
+
+          //Centro el mapa en las coordenadas
+          map.setView([lat, lon], 12);
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Lo siento, no se encontro ese lugar.",
+          });
+        }
+      });
   }
 });
 
 function searchCities(searchInput) {
-  fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${searchInput}`)
-      .then(response => response.json())
-      .then(data => {
-          var searchResultsContainer = document.getElementById('search-results');
-          searchResultsContainer.innerHTML = ''; // Limpiar los resultados anteriores
+  fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${searchInput}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var searchResultsContainer = document.getElementById("search-results");
+      searchResultsContainer.innerHTML = ""; // Limpiar los resultados anteriores
 
-          if (data && data.length > 0) {
-              data.forEach(city => {
-                  var cityName = city.display_name;
-                  var cityElement = document.createElement('div');
-                  cityElement.textContent = cityName;
-                  cityElement.classList.add('search-result-item');
-                  cityElement.addEventListener('click', function() {
-                      var lat = parseFloat(city.lat);
-                      var lon = parseFloat(city.lon);
-                      map.setView([lat, lon], 12); // Ajusta la vista del mapa a la ubicación de la ciudad
-                      clearSearchResults(); // Limpiar los resultados después de seleccionar una ciudad
-                  });
-                  searchResultsContainer.appendChild(cityElement);
-              });
-          } else {
-              var noResultsMessage = document.createElement('div');
-              noResultsMessage.textContent = 'No se encontraron ciudades.';
-              searchResultsContainer.appendChild(noResultsMessage);
-          }
-      })
-      .catch(error => {
-          console.error('Error al buscar las ciudades:', error);
-      });
+      if (data && data.length > 0) {
+        data.forEach((city) => {
+          var cityName = city.display_name;
+          var cityElement = document.createElement("div");
+          cityElement.textContent = cityName;
+          cityElement.classList.add("search-result-item");
+          cityElement.addEventListener("click", function () {
+            var lat = parseFloat(city.lat);
+            var lon = parseFloat(city.lon);
+            map.setView([lat, lon], 12); // Ajusta la vista del mapa a la ubicación de la ciudad
+            clearSearchResults(); // Limpiar los resultados después de seleccionar una ciudad
+          });
+          searchResultsContainer.appendChild(cityElement);
+        });
+      } else {
+        var noResultsMessage = document.createElement("div");
+        noResultsMessage.textContent = "No se encontraron ciudades.";
+        searchResultsContainer.appendChild(noResultsMessage);
+      }
+    })
+    .catch((error) => {
+      console.error("Error al buscar las ciudades:", error);
+    });
 }
 
 function clearSearchResults() {
-  var searchResultsContainer = document.getElementById('search-results');
-  searchResultsContainer.innerHTML = '';
+  var searchResultsContainer = document.getElementById("search-results");
+  searchResultsContainer.innerHTML = "";
 }
 // Agregar evento de clic al mapa
 map.on("click", onMapClick);
